9af7efeaabc7282b4729dac51bba892c8c8718bd
ggrillo93 <ggrillo93@gmail.com>
2024-09-12 14:57:54 +0100

    Add initial scripts for Chebyshev and Trigonometric series

---
+++ chebtrigexp.py
@@ -0,0 +1,122 @@
+from trigexp import *
+from chebpy.core.algorithms import vals2coeffs2, chebpts2
+from chebpy.core.chebtech import Chebtech
+
+class ChebTrigExpansion():
+    def __init__(self, TrigExpArr = None, ChebExpArr = None, parity = None, trig_type = None): # parity corresponds to the parity of the first Fourier coefficient
+
+        assert(not (type(TrigExpArr) == type(None) and (type(ChebExpArr) == type(None))))
+
+        if type(TrigExpArr) != None:
+
+            assert(all(isinstance(item, self.trig_type) for item in TrigExpArr)) # make sure all trigonometric expansions are of the same type
+            assert(all(item.deg == self.deg for item in TrigExpArr)) # make sure all expansion have the same degree (could pad with zeros though)
+            assert(len(TrigExpArr) % 2 == 0) # make sure the number of spatial samples is even
+            assert(type(parity) != type(None))
+
+            self.parity = parity
+            self.nFourier = TrigExpArr[0].deg + 1
+            self.nCheb = 2 * len(TrigExpArr) # half of these will be zero
+            self.trig_type = type(TrigExpArr[0])
+            self.TrigExpArr, self.TrigCoeffGrid, self.ChebCoeffGrid, self.ChebExpArr = self._initialize_with_TrigExp(TrigExpArr) # the new TrigExpArr will be 2 sided
+            
+        else:
+            self.nCheb = len(ChebExpArr[0].coeffs)
+
+            assert(self.nCheb % 2 == 0)
+            assert(trig_type == cosExpansion or trig_type == sinExpansion)
+
+            self.trig_type = trig_type
+            self.ChebExpArr = ChebExpArr
+            self.nFourier = len(ChebExpArr)
+            self.ChebCoeffGrid, self.TrigCoeffGrid, self.TrigExpArr = self._initialize_with_ChebExp()
+
+    def _initialize_with_TrigExp(self, TrigExpArr):
+
+        TrigExp2Side = np.zeros(self.nCheb, dtype = self.trig_type)
+        TrigExp2Side[self.nCheb // 2:] = np.copy(TrigExpArr)
+        TrigExp2Side[:self.nCheb // 2] = np.flip(TrigExpArr)
+
+        mult = np.ones(self.nFourier)
+        if self.parity == 'even':
+            mult[1::2] = -1
+        else:
+            mult[::2] = -1
+
+        if self.trig_type == cosExpansion:
+            for i in range(self.nCheb // 2):
+                TrigExp2Side[i] = cosExpansion(coeffs = TrigExp2Side[i].coeffs * mult)
+        else:
+            for i in range(self.nCheb // 2):
+                TrigExp2Side[i] = sinExpansion(coeffs = TrigExp2Side[i].coeffs * mult)
+        
+        TrigCoeffGrid = np.zeros([self.nCheb, self.nFourier])
+        for i in range(self.nCheb):
+            TrigCoeffGrid[i] = TrigExp2Side.coeffs
+        
+        ChebCoeffGrid = np.zeros([self.nCheb, self.nFourier])
+        ChebExpArr = np.zeros(self.nFourier, dtype = 'object')
+        for i in range(self.nFourier):
+            coeffs = vals2coeffs2(TrigCoeffGrid[:, i])
+            if (i % 2 == 0 and self.parity == 'even') or (i % 2 != 0 and self.parity == 'odd'):
+                coeffs[1::2] = 0
+            else:
+                coeffs[::2] = 0
+            ChebCoeffGrid[:, i] = coeffs
+            ChebExpArr[i] = Chebtech(coeffs)
+        
+        return TrigExp2Side, TrigCoeffGrid, ChebCoeffGrid, ChebExpArr
+        
+    def _initialize_with_ChebExp(self):
+
+        ChebCoeffGrid = np.zeros([self.nCheb, self.nFourier])
+
+        for i in range(self.nCheb):
+            ChebCoeffGrid[:, i] = self.ChebExpArr[i].coeffs
+
+        if np.allclose(ChebCoeffGrid[:, 0][1::2], 0):
+            self.parity = 'even'
+        else:
+            self.parity = 'odd'
+        
+        nodes = chebpts2(self.nCheb)
+        
+        TrigCoeffGrid = np.zeros([self.nCheb, self.nFourier])
+        for i in range(self.nFourier):
+            TrigCoeffGrid[:, i] = self.ChebExpArr[i](nodes)
+        
+        TrigExp2Side = np.zeros(self.nCheb, dtype = self.trig_type)
+        if self.trig_type == cosExpansion:
+            for i in range(self.nCheb):
+                TrigExp2Side[i] = cosExpansion(TrigCoeffGrid[i])
+        else:
+            for i in range(self.nCheb):
+                TrigExp2Side[i] = sinExpansion(TrigCoeffGrid[i])
+        
+        return ChebCoeffGrid, TrigCoeffGrid, TrigExp2Side
+    
+    def spatialDerivative(self):
+
+        newChebExpArr = np.zeros(self.nFourier, dtype = 'object')
+        for i in range(self.nFourier):
+            newChebExpArr[i] = self.ChebExpArr[i].diff()
+        
+        return ChebTrigExpansion(ChebExpArr = newChebExpArr, trig_type = self.trig_type)
+    
+    def angDerivative(self):
+
+        newTrigExpArr = np.zeros(self.nCheb // 2, dtype = 'object')
+        for i in range(self.nCheb // 2):
+            newTrigExpArr[i] = self.TrigExpArr[i + self.nCheb // 2].derivative()
+
+        newParity = 'even' if self.parity == 'odd' else 'odd'
+        return ChebTrigExpansion(TrigExpArr = newTrigExpArr, parity = newParity)
+    
+    def eval(self):
+
+        values = np.zeros([self.nCheb // 2, 2 * self.nFourier])
+
+        for i in range(self.nCheb // 2):
+            values[i] = self.TrigExpArr[i + self.nCheb // 2].evalGridFFT()
+
+        return values
---
+++ trigexp.py
@@ -0,0 +1,214 @@
+import numpy as np
+from scipy.fft import dct, dst, idct, idst
+
+class TrigExpansion:
+    def __init__(self, coeffs=None, thetas=None, vals=None, deg=None, useFFT=True, trig_type='cos'):
+        assert trig_type in ['cos', 'sin'], "trig_type must be 'cos' or 'sin'"
+        self.trig_type = trig_type
+        self.coeffs, self.deg = self._initialize_coeffs(coeffs, thetas, vals, deg, useFFT)
+
+    @classmethod
+    def zero_expansion(cls, deg=0):
+        """Initialize an expansion with all coefficients set to zero."""
+        coeffs = np.zeros(deg + 1)
+        return cls(coeffs=coeffs)
+
+    @classmethod
+    def unit_expansion(cls, deg=0):
+        """Initialize an expansion with the first harmonic equal to 1 and all others zero"""
+        coeffs = np.zeros(deg + 1)
+        if cls == cosExpansion:
+            coeffs[1] = 1
+        else:
+            coeffs[0] = 1
+        return cls(coeffs=coeffs)
+
+    def _initialize_coeffs(self, coeffs, thetas, vals, deg, useFFT):
+        if coeffs is not None:
+            return coeffs, len(coeffs) - 1
+
+        if useFFT:
+            fftCoeff = self._perform_fft(vals)
+            deg = deg if deg is not None else len(fftCoeff) - 1
+            return fftCoeff[:deg + 1], deg
+        else:
+            assert len(thetas) == len(vals), "Values for fit need to be equal to number of points"
+            return self.fit(thetas, vals, deg), deg
+
+    def _perform_fft(self, vals):
+        if self.trig_type == 'cos':
+            coeff = dct(vals) / len(vals)
+            coeff[0] *= 0.5
+            return coeff[::2]
+        else:
+            coeff = dst(vals) / len(vals)
+            return coeff[1::2]
+
+    def __add__(self, obj):
+        if isinstance(obj, TrigExpansion) and self.trig_type == obj.trig_type:
+            if self.trig_type == 'cos':
+                return cosExpansion(coeffs=self.coeffs + obj.coeffs)
+            else:
+                return sinExpansion(coeffs=self.coeffs + obj.coeffs)
+        elif isinstance(obj, (int, float, np.integer, np.floating)) and self.trig_type == 'cos':
+            newCoeff = np.copy(self.coeffs)
+            newCoeff[0] += obj
+            return cosExpansion(coeffs=newCoeff)
+        return NotImplemented
+    
+    def __radd__(self, obj):
+        """Handle addition when the instance is on the right side of the + operator."""
+        # Redirect to __add__ since addition is commutative
+        return self.__add__(obj)
+
+    def __sub__(self, obj):
+        if isinstance(obj, TrigExpansion) and self.trig_type == obj.trig_type:
+            if self.trig_type == 'cos':
+                return cosExpansion(coeffs=self.coeffs - obj.coeffs)
+            else:
+                return sinExpansion(coeffs=self.coeffs - obj.coeffs)
+        elif isinstance(obj, (int, float, np.integer, np.floating)) and self.trig_type == 'cos':
+            newCoeff = np.copy(self.coeffs)
+            newCoeff[0] -= obj
+            return cosExpansion(coeffs=newCoeff)
+        return NotImplemented
+
+    def __mul__(self, obj):
+        if isinstance(obj, TrigExpansion):
+            new_vals = self.evalGridFFT() * obj.evalGridFFT()
+            if self.trig_type == obj.trig_type:
+                return cosExpansion(vals=new_vals)
+            else:
+                return sinExpansion(vals=new_vals)
+        elif isinstance(obj, (int, float, np.integer, np.floating)):
+            newCoeff = np.copy(self.coeffs)
+            newCoeff *= obj
+            if self.trig_type == 'cos':
+                return cosExpansion(coeffs = newCoeff)
+            else:
+                return sinExpansion(coeffs = newCoeff)
+        return NotImplemented
+    
+    def __rmul__(self, obj):
+        """Handle multiplication when the instance is on the right side."""
+        # Call __mul__ with swapped order
+        return self.__mul__(obj)
+
+    def __truediv__(self, obj):
+        if isinstance(obj, TrigExpansion):
+            new_vals = self.evalGridFFT() / obj.evalGridFFT()
+            if self.trig_type == obj.trig_type:
+                return cosExpansion(vals=new_vals)
+            else:
+                return sinExpansion(vals=new_vals)
+        elif isinstance(obj, (int, float, np.integer, np.floating)):
+            newCoeff = np.copy(self.coeffs)
+            newCoeff /= obj
+            if self.trig_type == 'cos':
+                return cosExpansion(coeffs = newCoeff)
+            else:
+                return sinExpansion(coeffs = newCoeff)
+        return NotImplemented
+    
+    def __pow__(self, exponent):
+        if not isinstance(exponent, (int, np.integer)) and not np.allclose(exponent, 0.5):
+            raise ValueError("Exponent must be an integer or 0.5")
+        if np.allclose(exponent, 0):
+            return cosExpansion.unit_expansion(deg = self.deg)
+        elif np.allclose(exponent, 1):
+            return self
+        elif np.allclose(exponent, 0.5):
+            return self.sqrt()
+        else:
+            result = cosExpansion.zero_expansion(deg = self.deg) + 1
+            if exponent < 0:
+                for i in range(-exponent):
+                    result /= self
+            else:
+                for i in range(exponent):
+                    result *= self
+            return result
+        
+    def sqrt(self): # Highly suspect. Only use this when you know that the underlying series is the square of another series
+        if self.trig_type == 'cos':
+            new_vals = self.evalGridFFT() ** 0.5
+            return cosExpansion(vals = new_vals)
+        return NotImplemented # need to think about the sine case
+
+    def fit(self, thetas, vals, deg):
+        M = len(vals)
+        assert M >= deg + 1, "Number of values needs to be at least 2 * deg + 1"
+        A = np.zeros((M, deg + 1))
+        A[:, 0] = 1
+        for i in range(deg):
+            trig_func = np.cos if self.trig_type == 'cos' else np.sin
+            A[:, i + 1] = trig_func((i + 1) * thetas)
+        B = A.T @ A
+        if self.trig_type == 'cos':
+            return np.linalg.solve(B, A.T @ vals)
+        else:
+            return np.linalg.solve(B, A.T @ vals)[1:] # exclude constant term for sine since sine has no zero harmonic
+
+    def evalGrid(self, thetas):
+        res = 0
+        if self.trig_type == 'cos':
+            res += self.coeffs[0]
+            for i in range(1, len(self.coeffs)):
+                res += self.coeffs[i] * np.cos(i * thetas)
+        else:
+            for i in range(len(self.coeffs)):
+                res += self.coeffs[i] * np.sin((i+1) * thetas)
+        return res
+
+    def evalGridFFT(self): # idct and idst assume the presence of half frequencies. They are all zero in our case so we just set the respective coefficients to zero
+        if self.trig_type == 'cos':
+            newCoeff = np.zeros(2 * (self.deg + 1))
+            newCoeff[::2] = np.copy(self.coeffs)
+            newCoeff[0] *= 2
+            result = idct(newCoeff) * len(newCoeff)
+            return result
+        else:
+            newCoeff = np.zeros(2 * (self.deg + 1))
+            newCoeff[1::2] = self.coeffs
+            result = idst(newCoeff) * len(newCoeff)
+            return result
+
+    def eval(self, theta):
+        res = 0
+        if self.trig_type == 'cos':
+            res += self.coeffs[0]
+            for i in range(1, len(self.coeffs)):
+                res += self.coeffs[i] * np.cos(i * theta)
+        else:
+            for i in range(len(self.coeffs)):
+                res += self.coeffs[i] * np.sin((i+1) * theta)
+        return res
+
+    def derivative(self):
+        newCoeffs = np.zeros(self.deg + 1)
+        if self.trig_type == 'cos': # derivative will be a sine series of deg = deg - 1
+            for m in range(1, self.deg + 1):
+                newCoeffs[m - 1] = -m * self.coeffs[m]
+            return sinExpansion(coeffs = newCoeffs)
+        else:
+            for m in range(1, self.deg + 1):
+                newCoeffs[m] = m * self.coeffs[m-1]
+            return cosExpansion(coeffs=newCoeffs)
+
+    def integralContour(self):
+        return 2 * np.pi * self.coeffs[0]
+    
+    def copy(self):
+        if self.trig_type == 'cos':
+            return cosExpansion(coeffs = self.coeffs)
+        else:
+            return sinExpansion(coeffs = self.coeffs)
+
+class cosExpansion(TrigExpansion):
+    def __init__(self, coeffs=None, thetas=None, vals=None, deg=None, useDCT=True):
+        super().__init__(coeffs, thetas, vals, deg, useDCT, trig_type='cos')
+
+class sinExpansion(TrigExpansion):
+    def __init__(self, coeffs=None, thetas=None, vals=None, deg=None, useDST=True):
+        super().__init__(coeffs, thetas, vals, deg, useDST, trig_type='sin')
+            
